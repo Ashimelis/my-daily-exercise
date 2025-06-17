8e42da6aa363d3ff2ca3be3dc620c8a3a5c48985
shimelis <shimelisget2@gmail.com>
2025-06-16 18:11:00 -0700

    react router dom added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
-
-
+import Login from "./pages/Auth/Login";
+import {Route, Routes} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-      <h1> well come shimelis </h1>
+    <div className="h-[100vh] flex justify-center item-center">
+      <Routes>
+        <Route path='login' element={<Login/>}/>
+      </Routes>
+     
     </div>
   );
 }
--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
 function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <div>
+        <form>
+           <h1> Login</h1>
+        <div>
+          <input type='email'/>
+        <label htmlFor=''>Your Email</label>
+        </div>
+        <div>
+          <input type='password'/>
+        <label htmlFor=''>Your Password</label>
+        </div>
+        <div>
+          <div>
+            <input type='checkbox' name="id"/>
+            <label htmlFor='Remember Me'></label>
+          </div>
+          <span>Forget Password?</span>
+        </div>
+        <button type='submit'> Login</button>
+        </form>
+        <div>
+          <span>New Here?<Link to='Register'> create an Account</Link></span>
+        </div>
+      </div>
+    </div>
   )
 }
 
