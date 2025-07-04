85a81480689f1f32b0742f9d83b03eb761787ae2
shimelis <shimelisget2@gmail.com>
2025-07-04 01:22:06 -0700

    new feature tailwindcss added

--- Sew-trillion-app/Frontend/src/pages/HomePage/Home.js
+++ Sew-trillion-app/Frontend/src/pages/HomePage/Home.js
@@ -5,24 +5,26 @@ import image2 from "../../Assets/istockphoto3.webp"
 function Home() {
   return (
     
-     <div className='inline-block'>
-      <div className='flex'>
-        <div className='border-round shadow-lg'>
-        <h1 className='bg-slate-400 text-yellow-300'> Welcome to Sew-trillion</h1>
-        <h3 className='bg-slate-400 text-white'> We Build and Grow Together</h3>
+     <div className=' static container max-auto inline-block'>
+      <div className='flex box-border float-left'>
+        <div className='absolute top-0 left-0 border-round shadow-lg'>
+        <h1 className='bg-slate-400 relative text-yellow-300'> Welcome to Sew-trillion</h1>
+        <h3 className='bg-slate-400 relative text-white'> We Build and Grow Together</h3>
       </div>
       <div className='justify-start w-60 h-20 object-fill'>
-      <img src={image} alt='welcome to Sew-trillion'/> 
+      <img className='float-right' src={image} alt='welcome to Sew-trillion'/> 
     </div>
      </div>
-      
-       <div className='border-round shadow-lg'>
+      <div className='flex box-border float-right'>
+        <div className='border-round shadow-lg'>
         <h1 className='bg-slate-400 text-yellow-400'> Invest in your future</h1>
         <h3 className='bg-slate-400 text-white'> Save your money</h3>
       </div>
       <div className='justify-start w-60 h-20 object-fill'>
-      <img src={image2} alt='welcome to Sew-trillion'/> 
+      <img className='float-left' src={image2} alt='welcome to Sew-trillion'/> 
     </div>
+      </div>
+       
     <div className='inline'>
       <div>
       <h> Top Rated products</h>
