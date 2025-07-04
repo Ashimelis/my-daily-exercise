81d7311e98a9b256088c4e26023b987fea2f77ca
shimelis <shimelisget2@gmail.com>
2025-07-04 00:21:15 -0700

    Ne picture added

--- Sew-trillion-app/Frontend/src/pages/HomePage/Home.js
+++ Sew-trillion-app/Frontend/src/pages/HomePage/Home.js
@@ -4,22 +4,27 @@ import image2 from "../../Assets/istockphoto3.webp"
 
 function Home() {
   return (
-    <div  className='inline-block relative gap-8 px-6 py-6 border-round shadow-lg'>
-      <div className='border-round shadow-lg'>
+    
+     <div className='inline-block'>
+      <div className='flex'>
+        <div className='border-round shadow-lg'>
         <h1 className='bg-slate-400 text-yellow-300'> Welcome to Sew-trillion</h1>
         <h3 className='bg-slate-400 text-white'> We Build and Grow Together</h3>
       </div>
       <div className='justify-start w-60 h-20 object-fill'>
       <img src={image} alt='welcome to Sew-trillion'/> 
     </div>
-    <div className='border-round shadow-lg'>
+     </div>
+      
+       <div className='border-round shadow-lg'>
         <h1 className='bg-slate-400 text-yellow-400'> Invest in your future</h1>
         <h3 className='bg-slate-400 text-white'> Save your money</h3>
       </div>
       <div className='justify-start w-60 h-20 object-fill'>
       <img src={image2} alt='welcome to Sew-trillion'/> 
     </div>
-    <div>
+    <div className='inline'>
+      <div>
       <h> Top Rated products</h>
       <div className='flex'>
         <div>
@@ -40,8 +45,9 @@ function Home() {
       </div>
     </div>
     <div>
+      <div>
       <h1> Best sell Products</h1>
-      <div className='flex'>
+      <div div className='flex'>
         <div>
           <img alt=''/>
         </div>
@@ -57,12 +63,20 @@ function Home() {
         <div>
           <img alt=''/>
         </div>
-        v
+
       </div>
-    </div>
-    </div>
+      </div>
+      </div>
+      </div>
+      </div>
+      
     
   )
 }
 
 export default Home
+
+      
+     
+    
+   
--- Sew-trillion-app/Frontend/src/pages/LoanService/LoanService.js
+++ Sew-trillion-app/Frontend/src/pages/LoanService/LoanService.js
@@ -3,12 +3,9 @@ import React from 'react'
 function LoanService() {
   return (
     <div>
-      <button className="group bg-yellow-500 focus:bg-yellow-600 ...">
-  <svg className="text-white group-focus:text-yellow-300 ..."></svg>
-  Bookmark
-</button>
+      
     </div>
-  )
+);
 }
 
 export default LoanService
